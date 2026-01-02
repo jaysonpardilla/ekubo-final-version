35a2d362ddaf61c82af47ab0cf42f45107a2e9e5
Jayson-prog <javerdomaraya7@gmail.com>
2026-01-02 14:13:07 +0800

    Fix DJANGO_SETTINGS_MODULE paths to core.core.settings and Daphne path

--- core/core/asgi.py
+++ core/core/asgi.py
@@ -6,7 +6,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 
 # Set the Django settings module
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.core.settings')
 
 # This line is crucial. It sets up Django's application registry.
 # It should be called after the settings module is defined.
