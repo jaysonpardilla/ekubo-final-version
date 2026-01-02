ccce8524253b2dad9264924066bf2f0ace3a2f15
Jayson-prog <javerdomaraya7@gmail.com>
2026-01-02 16:50:45 +0800

    add core

--- core/manage_business/apps.py
+++ core/manage_business/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ManageBusinessConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'core.manage_business'
+    name = 'manage_business'
