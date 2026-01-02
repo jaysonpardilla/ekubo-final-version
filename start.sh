d9efd094167b1f941782f2e4541194865f6c497c
Jayson-prog <javerdomaraya7@gmail.com>
2026-01-02 13:46:06 +0800

    Products: add uuid_id fields and convert unsafe id

--- start.sh
+++ start.sh
@@ -6,4 +6,4 @@ python core/manage.py migrate --noinput
 python core/manage.py collectstatic --noinput
 
 # Start ASGI server (Daphne). Render provides $PORT.
-exec daphne -b 0.0.0.0 -p ${PORT:-8000} core.asgi:application
+exec daphne -b 0.0.0.0 -p ${PORT:-8000} core.core.asgi:application
