07e4a73020c6f522a6b357164f749e179b747bd4
Jayson-prog <javerdomaraya7@gmail.com>
2026-01-03 10:52:48 +0800

    Allow int or uuid PKs for shop URLs (use <str:pk>)

--- core/shops/urls.py
+++ core/shops/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = 'shops'
 urlpatterns = [
     path('', views.shops_list, name='shops_list'),
-    path('update-shop-informations/<uuid:pk>', views.update_shop, name='update_shop'), 
-    path('view-shop/<uuid:pk>/', views.view_shop, name='view_shop'),
+    path('update-shop-informations/<str:pk>/', views.update_shop, name='update_shop'),
+    path('view-shop/<str:pk>/', views.view_shop, name='view_shop'),
 ]
 
 
